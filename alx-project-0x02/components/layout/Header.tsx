709c544ea669bb590877858df475844024ce480d
Usytech01 <usmandioo@yahoo.com>
2025-09-23 19:38:11 +0100

    adding task 2

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <Link href="/home">Home</Link>
+                <Link href="about">About</Link>
+            </nav>
+        </header>
+    );
+}
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+    return(
+        <div>
+            <h1>this is Home Page</h1>
+            <p>this is my first custom route on next.js</p>
+        </div>
+    );
+}
