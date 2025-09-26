6c1e7aec8473c728c1237fe9afd75737845ceedf
Usytech01 <usmandioo@yahoo.com>
2025-09-26 12:17:32 +0100

    Correction

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -3,9 +3,11 @@ import { ButtonProps } from "@/interfaces";
 
 export default function Button({
   label,
+  size = "medium",
+  shape = "rounded-md",
   onClick,
 }: ButtonProps) {
     return(
-        <button onClick={onClick} className="p-3 rounded-md text-white cursor-pointer ">{label}</button>
+        <button onClick={onClick} className="p-3 rounded-md text-white cursor-pointer">{label}</button>
     )
 }
