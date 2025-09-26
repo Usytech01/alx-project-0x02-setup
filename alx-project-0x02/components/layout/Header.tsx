c1b129aa043fa260175211e31821a9a87eb41b1e
Usytech01 <usmandioo@yahoo.com>
2025-09-26 15:45:05 +0100

    adding task6

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,10 +4,11 @@ import React from "react";
 
 export default function Header(){
     return(
-        <header>
-            <nav>
-                <Link href="/home">Home</Link>
-                <Link href="about">About</Link>
+        <header className="bg-blue-100 shadow-md ">
+            <nav className="flex gap-4">
+                <Link href="/home" className="hover:underline text-blue-600">Home</Link>
+                <Link href="/home" className="hover:underline text-blue-600">About</Link>
+                <Link href="/home" className="hover:underline text-blue-600">Posts</Link>
             </nav>
         </header>
     );
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage(){
+    return(
+        <>
+        <Header/>
+        <main className="p-4">
+            <h1 className="text-2x1 font-bold">Posts</h1>
+            <p>here you can see all posts</p>
+        </main>
+        </>
+    )
+}
